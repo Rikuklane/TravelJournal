7c2f60e971f190227c2840be621693216fe202f4
Marilin Kuusk <marilinkuusk98@gmail.com>
2022-12-06 22:04:13 +0200

    Changed gallery to Point of Interest Maps View
    Maps View crashes when there are no PoI-s near user, there is a null pointer exception. The id-s of trips are still messed after deleting one trip

--- app/src/main/java/com/example/traveljournal/MainActivity.kt
+++ app/src/main/java/com/example/traveljournal/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_trips, R.id.nav_gallery, R.id.nav_packing, R.id.nav_document
+                R.id.nav_trips, R.id.nav_map, R.id.nav_packing, R.id.nav_document
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/traveljournal/ui/map/MapViewModel.kt
+++ app/src/main/java/com/example/traveljournal/ui/map/MapViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.traveljournal.ui.gallery
+package com.example.traveljournal.ui.map
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class MapViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is map Fragment"
     }
     val text: LiveData<String> = _text
 }
